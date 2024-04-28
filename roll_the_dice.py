19bd3f2421e1be6a77b6ee32c2dee1dce7980c53
Neeraj-perihar <168361882+Neeraj-perihar@users.noreply.github.com>
2024-04-28 22:16:51 +0530

    ROLL THE DICE

---
+++ roll_the_dice.py
@@ -0,0 +1,16 @@
+import random
+scorey=0 
+scorec=0 
+while True:
+    x= random.randrange(1,7)
+    y= random.randrange(1,7)
+    print("you got:",y,"AND","engine got:",x)
+    scorey+=y
+    scorec+=x
+    cont=input("Do you wish to continue type y/n: ").lower()
+    if cont=="y":
+      continue 
+    else:
+       break
+       if scorey>scorec:
+              print("You win. The score is: ",scorey,"-",scorec ) elif scorey
