4a6883c8d9832e100eb94502c8fbdb3869014d3c
Neeraj-perihar <168361882+Neeraj-perihar@users.noreply.github.com>
2024-04-28 22:14:32 +0530

    calculator

---
+++ calculator.py
@@ -0,0 +1,12 @@
+n = list(map(str,input("Enter the equation saperated by space ex: 45 * 84: ").split()))
+if n[1]=='+':
+             print(*n,'=',float(n[0]) + float(n[2]))
+elif n[1]=='-':
+                print(*n,'=',float(n[0]) - float(n[2])) 
+elif n[1]=='*':
+                 print(*n,'=',float(n[0]) * float(n[2])) 
+
+elif n[1]=='/':
+                 print(*n,'=',float(n[0]) / float(n[2]))
+else:
+                 print("enter valid operator i.e. '+,-,*,/'")
